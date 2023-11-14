2aff5ddc84b4ae4dd9520de4b01749c37e192419
Felipe-M94 <felipe.silva.mendonca@gmail.com>
2023-11-14 09:50:19 -0300

    listas

---
+++ listas/app.py
@@ -0,0 +1,26 @@
+nota1 = 7.9
+nota2 = 9.7
+nota3 = 8.2
+
+notas = [7.9, 9.7, 8.2]
+
+# Criando Listas
+
+lista = []
+lista = list()
+lista = [29, 'Felipe', 3.1020, True]
+lista_de_listas = [10, [1, 2, 3]]
+
+# Slices
+
+lista = [10, 50, 30, 40, 25, 60, 5]
+
+print(lista[0:3])
+print(lista[3:6])
+
+
+# Iterações com FOR
+
+for i in lista:
+    print(i)
+
