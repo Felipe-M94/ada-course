d8712e8f1629f373b5ce30ade887da6443765113
Felipe-M94 <felipe.silva.mendonca@gmail.com>
2023-11-14 12:01:13 -0300

    Dicionarios

---
+++ dicionario/app.py
@@ -0,0 +1,22 @@
+# Dicionarios
+
+dicionario = {}
+dicionario = dict()
+
+dicionario = {
+    'nome': 'Felipe',
+    'idade': 29,
+    'altura': 1.69
+}
+
+print(dicionario['idade'])
+
+# Add Elementos Dicionario
+
+dicionario['Programador'] = True
+
+# Alterando Dicionário
+
+for i in dicionario:
+    print(i, dicionario['nome'] )
+
