af561212b850d4df96c87e1a69b3043c8b23d75d
Felipe-M94 <felipe.silva.mendonca@gmail.com>
2023-11-14 10:07:06 -0300

    Metodos

---
+++ metodos/app.py
@@ -0,0 +1,55 @@
+lista = [10, 50, 30, 40, 25, 60, 5]
+
+# append
+
+lista.append(3)
+
+# insert
+
+lista.insert(2, 10)
+
+# extend
+
+lista2 = [1, 2, 3]
+
+lista.extend(lista2)
+
+# pop
+
+lista.pop()
+
+# remove
+
+lista.remove(3)
+
+# count
+
+lista.count(2)
+
+# index
+
+lista.index()
+
+# sort
+
+lista.sort()
+
+
+# Funções Para Listas
+
+# Len
+
+print(len(lista))
+
+# sum
+
+print(sum(lista))
+
+# max
+
+print(max(lista))
+
+# min 
+
+print(min(lista))
+
